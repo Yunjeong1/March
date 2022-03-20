803c1d1846d28aa6f316f3b78720e0a78e243319
Yunjeong1 <8768moon@naver.com>
2022-03-21 01:27:25 +0900

    third commit_Swiper&Isotope

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
+//Swiper 적용
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  const btnCall = document.querySelector(".btnCall"); 
+  const menuMo = document.querySelector(".menuMo"); 
+  
+  
+  btnCall.onclick = function(e){
+      //링크이동금지 
+      e.preventDefault(); 
+  
+      //btnCall에 on이 있으면 제거, 없으면 추가 
+      btnCall.classList.toggle("on"); 
+      //menuMo에 on이 있으면 제거, 없으면 추가 
+      menuMo.classList.toggle("on"); 
+  }
 
-const btnCall = document.querySelector(".btnCall"); 
-const menuMo = document.querySelector(".menuMo"); 
-
-
-btnCall.onclick = function(e){
-    //링크이동금지 
-    e.preventDefault(); 
-
-    //btnCall에 on이 있으면 제거, 없으면 추가 
-    btnCall.classList.toggle("on"); 
-    //menuMo에 on이 있으면 제거, 없으면 추가 
-    menuMo.classList.toggle("on"); 
-}
---
+++ js/sub.js
@@ -0,0 +1,35 @@
+//isotope 적용
+window.addEventListener("load", ()=>{
+    const grid = new Isotope(".wrap", { //배치할 요소의 부모요소명
+        itemSelector : "article", //배치할 요소명
+        columnWidth: "article", //너비값의 기준이 될 요소나 클래스명
+        transitionDuration: "0.8s" //화면 재배치시 요소의 움직임 속도
+    })
+  
+    //sort버튼 변수에 저장
+    const btns = document.querySelectorAll(".women .inner ul li");
+  
+    for(let el of btns){
+        
+    //sort버튼 클릭했을때
+    el.addEventListener("click", e=>{
+        
+        e.preventDefault();
+  
+        
+        //클릭한 sort버튼의 자식인 a의 href값 구해서 저장
+        const sort = e.currentTarget.querySelector("a").getAttribute("href");
+  
+        //sort값에 따라 필터링 및 재배치
+        grid.arrange({
+            filter:sort
+        });
+  
+        //모든 버튼 활성화
+        for(let el of btns){
+            el.classList.remove("on");
+        }
+        e.currentTarget.classList.add("on");
+    })
+   }
+  })
