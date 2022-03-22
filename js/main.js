bdec1ad5ef761d9d95833546dc49807e75a26198
Yunjeong1 <8768moon@naver.com>
2022-03-23 01:34:02 +0900

    anime_assignment

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
   
+//햄버거메뉴 버튼 클릭시
   const btnCall = document.querySelector(".btnCall"); 
   const menuMo = document.querySelector(".menuMo"); 
-  
   
   btnCall.onclick = function(e){
       //링크이동금지 
@@ -31,3 +31,99 @@ var swiper = new Swiper(".mySwiper", {
       menuMo.classList.toggle("on"); 
   }
 
+//news의 view more 버튼 클릭시
+const body = document.querySelector("body");
+const news = document. querySelector("#news");
+const btnOpen = document.querySelectorAll(".btnOpen");
+console.log(btnOpen);
+const aside = document.querySelector("aside");
+const popUp = aside.querySelector(".popUp"); 
+const inner = popUp.querySelector(".inner");
+const _top = popUp.querySelector(".top"); 
+const _right = popUp.querySelector(".right"); 
+const _bottom = popUp.querySelector(".bottom"); 
+const _left = popUp.querySelector(".left"); 
+const speed = 500;
+const btnClose = popUp.querySelector(".close");
+
+for(let i=0; i<8; i++){
+  btnOpen[i].addEventListener("click", e=>{
+    e.preventDefault();
+    aside.style.display = "block";
+    popUp.style.display = "block";
+    body.style.overflow = "hidden";
+    swiper.autoplay.stop();
+
+    new Anime(_top,{
+      prop: "width",
+      value: "100%",
+      duration: speed,
+      callback:()=>{
+        new Anime(_right,{
+          prop: "height", 
+          value :"100%", 
+          duration: speed, 
+          callback :()=>{
+              new Anime(_bottom,{
+                  prop:"width", 
+                  value:"100%", 
+                  duration:speed, 
+                  callback:()=>{
+                      new Anime(_left, {
+                          prop:"height", 
+                          value:"100%", 
+                          duration:speed, 
+                          callback :()=>{
+                              new Anime(inner,{
+                                  prop:"opacity",
+                                  value:1, 
+                                  duration:speed
+                              })
+                          }
+                      })
+                  }
+              })
+            }
+         })
+       }
+    })
+  })
+}
+
+btnClose.addEventListener("click", e=>{
+  e.preventDefault();
+  swiper.autoplay.start();
+  body.style.overflow = "visible";
+
+  new Anime(inner,{
+    prop:"opacity", 
+    value:0, 
+    duration:speed, 
+    callback:()=>{
+        new Anime(_top,{
+            prop:"width", 
+            value:0, 
+            duration:speed
+        });
+        new Anime(_right,{
+            prop:"height", 
+            value:0, 
+            duration:speed
+        })
+        new Anime(_bottom,{
+            prop:"width", 
+            value:0, 
+            duration:speed
+        })
+        new Anime(_left,{
+            prop:"height", 
+            value:0, 
+            duration:speed,
+            callback :()=>{
+              aside.style.display = "none";
+              popUp.style.display = "none";
+            }
+        })
+    }
+})
+})
